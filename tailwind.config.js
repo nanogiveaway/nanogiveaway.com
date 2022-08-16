e31c88c5929b7b4d7d908af3b4ab7bf8b5efd16b
epiphany <epiphanyfororder@gmail.com>
2022-08-17 00:36:31 +0300

    tailwind config, remove unneeded

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#209CE9",
+        dark: "#20204C",
+        light: "#F4FAFF",
+        gray: "#676686",
+        black: "#070707",
+        white: "#FFFFFF",
+        "gray-50": "#F9FAFB",
+        "gray-100": "#F3F4F6",
+        "gray-200": "#E5E7EB",
+        "gray-300": "#D1D5DB",
+        "gray-400": "#9CA3AF",
+        "gray-500": "#6B7280",
+        "gray-600": "#4B5563",
+        "gray-700": "#374151",
+        "gray-800": "#1F2937",
+        "gray-900": "#111827",
+      },
+    },
   },
   plugins: [],
-}
+};
