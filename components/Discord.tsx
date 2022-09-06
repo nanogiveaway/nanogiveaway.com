de7c83126e835b7a6ea4d7625d6e841f4e05d657
Arda Sevinç <ardasevinc.technical@gmail.com>
2022-09-06 23:56:05 +0300

    add iframe title

--- components/Discord.tsx
+++ components/Discord.tsx
@@ -1,6 +1,7 @@
 const Discord = ({ className = "" }) => {
   return (
     <iframe
+      title="Discord widget for Nano Giveaway server"
       src="https://discord.com/widget?id=626145793413218342&theme=dark"
       className={`${className}`}
       frameBorder="0"
